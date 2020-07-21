d9598c910b6ed2e3d362b722c18c0e6bdf2cd8ad
Alexey Vatolin <vatolinalex@gmail.com>
2020-07-22 00:27:53 +0500

    Single thread dataset

--- src/transformers/data/datasets/language_modeling.py
+++ src/transformers/data/datasets/language_modeling.py
@@ -185,3 +185,35 @@ class ParallelTextDataset(Dataset):
         return torch.tensor(self.examples[i], dtype=torch.long)
 
 
+class LineByLineCacheTextDataset(Dataset):
+    """
+    This will be superseded by a framework-agnostic approach
+    soon.
+    """
+
+    def __init__(self, tokenizer: PreTrainedTokenizer, file_path: str, block_size: int):
+        assert os.path.isfile(file_path)
+        # Here, we do not cache the features, operating under the assumption
+        # that we will soon use fast multithreaded tokenizers from the
+        # `tokenizers` repo everywhere =)
+        logger.info("Creating features from dataset file at %s", file_path)
+
+        directory, filename = os.path.split(file_path)
+        cached_features_file = os.path.join(
+            directory, "cached_lm_{}_{}_{}.hdf5".format(tokenizer.__class__.__name__, str(block_size), filename, ),
+        )
+
+        with open(file_path, encoding="utf-8") as f:
+            lines = [line for line in f.read().splitlines() if (len(line) > 0 and not line.isspace())]
+        with h5py.File(cached_features_file, 'w') as f:
+            for i, line in enumerate(lines):
+                batch_encoding = tokenizer(line, add_special_tokens=True, truncation=True, max_length=block_size)
+                f[str(i)] = batch_encoding["input_ids"]
+        self.examples = h5py.File(cached_features_file, 'r')
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, i) -> torch.Tensor:
+        return torch.tensor(self.examples[str(i)], dtype=torch.long)
+
